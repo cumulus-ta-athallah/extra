8fe9cd4353e80efaf83aea54a86f95590785a17e
Muhammad Athallah <mhd.athallah@gmail.com>
2024-05-01 01:07:20 +0700

    upgrade: react and yarn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
